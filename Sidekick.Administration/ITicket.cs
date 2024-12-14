c3c599f6de8a400758f4798f92511f89211e7729
tacosontitan <contact@tacosontitan.com>
2024-12-14 10:33:38 -0600

    Add ticket and ticket service

---
+++ Sidekick.Administration/ITicket.cs
@@ -0,0 +1,12 @@
+﻿namespace Sidekick.Administration;
+
+/// <summary>
+/// Defines members to represent a basic ticket.
+/// </summary>
+public interface ITicket
+{
+    /// <summary>
+    /// The unique identifier for the ticket.
+    /// </summary>
+    public Guid Id { get; }
+}
