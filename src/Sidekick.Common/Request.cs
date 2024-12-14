5af9c00f12669926af24d3737dfbc89112c30ab9
tacosontitan <contact@tacosontitan.com>
2024-12-14 10:20:11 -0600

    Create request model

---
+++ src/Sidekick.Common/Request.cs
@@ -0,0 +1,17 @@
+﻿namespace Sidekick.Common;
+
+/// <summary>
+/// Represents a request within the context of the executing application.
+/// </summary>
+public abstract record Request
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for the request.
+    /// </summary>
+    public Guid Id { get; init; }
+
+    /// <summary>
+    /// Gets the date and time the request was created.
+    /// </summary>
+    public DateTimeOffset DateCreated { get; } = DateTimeOffset.UtcNow;
+}
