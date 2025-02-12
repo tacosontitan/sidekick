a0f4f9ab6ddde70ace1f3c1c5eec10b155e61079
Hazel <contact@tacosontitan.com>
2025-02-12 08:10:44 -0500

    Add ticket and ticket service

---
+++ Sidekick.Administration/ITicketService.cs
@@ -0,0 +1,34 @@
+﻿namespace Sidekick.Administration;
+
+/// <summary>
+/// Defines members for interacting with tickets.
+/// </summary>
+public interface ITicketService
+{
+    /// <summary>
+    /// Adds a comment to the specified ticket.
+    /// </summary>
+    /// <param name="ticket">The ticket to add specified <paramref name="comment"/> to.</param>
+    /// <param name="comment">The comment to add to the ticket.</param>
+    /// <param name="cancellationToken">A token for cancelling the operation.</param>
+    /// <returns>A task describing the state of the operation.</returns>
+    public Task AddComment(
+        ITicket ticket,
+        string comment,
+        CancellationToken cancellationToken);
+
+    /// <summary>
+    ///     Adds the specified <paramref name="comment"/> and <paramref name="attachment"/> to the specified
+    ///     <paramref name="ticket"/>.
+    /// </summary>
+    /// <param name="ticket">The ticket to add specified <paramref name="comment"/> to.</param>
+    /// <param name="comment">The comment to add to the ticket.</param>
+    /// <param name="attachment">The attachment to add to the ticket as part of the <paramref name="comment"/>.</param>
+    /// <param name="cancellationToken">A token for cancelling the operation.</param>
+    /// <returns>A task describing the state of the operation.</returns>
+    public Task AddComment(
+        ITicket ticket,
+        string comment,
+        byte[] attachment,
+        CancellationToken cancellationToken);
+}
