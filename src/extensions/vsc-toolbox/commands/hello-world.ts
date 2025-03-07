b045da0820c4b019fa3892e7b79193c6e03d9efe
Hazel <contact@tacosontitan.com>
2025-03-07 08:11:04 -0500

    Add temp code to remember how to get configurations

--- src/extensions/vsc-toolbox/commands/hello-world.ts
+++ src/extensions/vsc-toolbox/commands/hello-world.ts
@@ -5,17 +5,15 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "toolbox" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+	// TODO: remove this and create a service for getting configurations in a more concrete way
+	// this was only added so I remember how to get configurations in the future
+	const configuration = vscode.workspace.getConfiguration('hazl.toolbox');
+	const personalAccessToken = configuration.get<string | null>("hazl.toolbox.azure.devops.pat");
+	console.log(personalAccessToken);
+		
 	const disposable = vscode.commands.registerCommand('toolbox.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from toolbox!');
 	});
 
